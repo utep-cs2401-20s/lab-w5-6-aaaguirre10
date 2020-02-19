1f2e1136ef6a2bb4f2a912fc4bebc91d336181b1
Antonio Aguirre <antonioaguirre@Antonios-MacBook-Pro.local>
2020-02-19 14:57:22 -0700

    created methods and constructor definitions.

---
+++ code/src/SnakeGame.java
@@ -0,0 +1,51 @@
+public class SnakeGame {
+
+    //Elements
+    private boolean[][] game(){
+
+    }
+    private int[] headPosition(){
+
+
+    }
+    private static int exhaustiveChecks(){
+
+    }
+    private static int recurviseChecks(){
+
+    }
+//Constructos
+    public int[] gameboard(3,3){
+
+    }
+    public boolean[][] snakeHead(){
+
+    }
+//Method
+    public int[] findTailExhaustive(){
+
+    }
+    private int[] findTailRecursive(int[] currentPosition, int[]previousPosition){
+        //hi
+    }
+
+    private void resetCounters(){
+
+    }
+    private static int getRecursiveChecks(){
+
+    }
+
+    private static  int getExhaustiveChecks(){
+
+    }
+
+
+
+
+
+
+
+
+
+}
